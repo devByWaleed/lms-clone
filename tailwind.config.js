6567e1aeef77269d407676a381b8255d42c9312a
Waleed Ahmed <163301387+waleed1857@users.noreply.github.com>
2024-09-14 12:25:23 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary' : '#716ACA'
+      },
+
+      height: {
+        '1st' : '93vh' ,
+        '2nd' : '7vh'
+      }
+
+    },
+  },
+  plugins: [],
+}
+
